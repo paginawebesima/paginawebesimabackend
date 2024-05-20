8228a035f3c5d80e112215fc67f107c582195902
trejo903 <paginawebesima@gmail.com>
2024-05-19 19:52:54 -0600

    Second Commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv'
+import { conexionBaseDeDatos } from "./config/db";
 
+dotenv.config();
+
+conexionBaseDeDatos();
 const servidor = express();
 
 
