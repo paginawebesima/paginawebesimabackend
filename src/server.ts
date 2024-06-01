be040ec9ebf69f00fd4001870f9dd305d75f8150
trejo903 <paginawebesima@gmail.com>
2024-06-01 10:02:17 -0600

    Commit 01 de junio

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { conexionBaseDeDatos } from "./config/db";
 
 import rutas from './routes/proyectorutas'
+import { configuracionCors } from "./config/cors";
 
 dotenv.config();
 
 conexionBaseDeDatos();
 const servidor = express();
+servidor.use(cors(configuracionCors))
 servidor.use(express.json())
 servidor.use('/api/proyecto',rutas)
 
