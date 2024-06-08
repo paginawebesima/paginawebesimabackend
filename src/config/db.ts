60d0b608b935151a5013dc196e4006cc25b30142
MARCO-ROSAS\markr <marco.rosas.isw@unipolidgo.edu.mx>
2024-06-08 15:34:56 -0600

    Agregar cambios

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,20 +1,5 @@
 import mongoose from 'mongoose'
 import colors from 'colors'
-<<<<<<< HEAD
-import { exit } from 'node:process'
-
-export const connection = mongoose.connection;
-
-export const conexionBaseDeDatos = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL);
-        console.log(colors.magenta.yellow(`Base de datos conectado ${connection.port} ${connection.host}`));
-    } catch (error) {
-        console.log('Error al conectar la base de datos');
-        exit(1);
-    }
-};
-=======
 import {exit} from 'node:process'
 export const conexionBaseDeDatos=async()=>{
     try {
@@ -25,4 +10,3 @@ export const conexionBaseDeDatos=async()=>{
         exit(1)
     }
 }
->>>>>>> be040ec9ebf69f00fd4001870f9dd305d75f8150
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,17 @@
 import express from "express";
 import dotenv from 'dotenv'
-<<<<<<< HEAD
-import { conexionBaseDeDatos } from "./config/db";
-
-=======
 import cors from 'cors'
 import { conexionBaseDeDatos } from "./config/db";
 
 import rutas from './routes/proyectorutas'
 import { configuracionCors } from "./config/cors";
 
->>>>>>> be040ec9ebf69f00fd4001870f9dd305d75f8150
 dotenv.config();
 
 conexionBaseDeDatos();
 const servidor = express();
-<<<<<<< HEAD
-
-=======
-servidor.use(cors(configuracionCors))
+// servidor.use(cors(configuracionCors))
 servidor.use(express.json())
 servidor.use('/api/proyecto',rutas)
->>>>>>> be040ec9ebf69f00fd4001870f9dd305d75f8150
 
 export default servidor;
