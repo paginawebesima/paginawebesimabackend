595a4f85d9f59d934cc5f5ea7f52d2884b42f44b
trejo903 <paginawebesima@gmail.com>
2024-06-17 20:42:37 -0600

    17 de junio 8:42pm

--- src/routes/proyectorutas.ts
+++ src/routes/proyectorutas.ts
@@ -106,7 +106,19 @@ rutas.delete('/eliminarPrestamo/:id',
     handleInputErrors,
     PrestamosControlador.eliminarPrestamo
 )
-
+rutas.get("/", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>Soy un proyecto Back end en vercel</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse);
+  });
 
 
 export default rutas;
